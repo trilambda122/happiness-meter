d387ac9d518dc824d1a3d05ea0a6152837741c5c
trilambda122 <trilambda122>
2021-04-23 09:07:50 -0400

    post routes and get find by id completed

--- api/routes/happy.js
+++ api/routes/happy.js
@@ -20,7 +20,13 @@ router.get('/:happyId', (req, res, next) => {
     .exec()
     .then((doc) => {
       console.log(doc);
-      res.status(200).json(doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({
+          message: 'No document found for that ID',
+        });
+      }
     })
     .catch((err) => {
       console.log(err);
